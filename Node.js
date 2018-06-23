29f1d4dfadcd8c23f79ad3aac94b56cfb3346f4b
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-06-23 12:16:00 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-  message.channel.send(message.content)
+  message.channel.send(message.content);
 });
 
 client.login(process.env.token);
