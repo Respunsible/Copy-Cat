0006786479f2bb5d38eb0b4152b138a91bd6d513
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-06-23 12:52:45 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const delay = require('delay');
 
 client.on('message', msg => {
+  await delay(1000);
   if (msg.author.id !== '460110841606570005') {
     msg.channel.send(msg.content);
   }
