52237117293c866aaa12fc381e2e117f432fc5c3
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-06-23 12:59:06 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,13 +1,9 @@
-$ npm install delay
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const delay = require('delay');
 
 client.on('message', msg => {
-  await delay(1000);
   if (msg.author.id !== '460110841606570005') {
-    msg.channel.send(msg.content);
+    msg.channel.send(msg.content filter, { time: 15000 });
   }
 });
 
