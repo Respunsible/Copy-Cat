8502bf23637b472a1c5287a11acaf67c83471a74
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-06-23 13:06:47 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -3,7 +3,9 @@ const client = new Discord.Client();
 
 client.on('message', msg => {
   if (msg.author.id !== '460110841606570005') {
-    msg.channel.send(msg.content filter, { time: 15000 });
+    var interval = setInterval (function () {
+      msg.channel.send(msg.content);
+    }, 1 * 100); 
   }
 });
 
